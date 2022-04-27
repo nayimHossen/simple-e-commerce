1332acbda0d6b7e42c196ec29d81f84c1e237eb3
Nayim hossen <nayimh2015@gmail.com>
2022-04-27 17:06:38 +0600

    shoping cart emplemented and add localStorage

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -13,16 +14,31 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products]);
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     };
 
     return (
         <div className='shop-container'>
             <div className="product-container">
                 {
-                    products.map(product => <Product
+                    products.slice(0, 12).map(product => <Product
                         key={product.id}
                         product={product}
                         handleAddToCart={handleAddToCart}
