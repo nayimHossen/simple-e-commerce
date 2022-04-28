bdb5fc0b516428d5a87fab945a5340f3a1d0d958
Nayim hossen <nayimh2015@gmail.com>
2022-04-28 23:06:15 +0600

    signUp and login system implemented

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password);
+    };
+
+    if (user) {
+        navigate('/orders')
+    }
+
     return (
         <div className='form-container'>
             <div className='input-container'>
                 <div>
                     <h2 className='form-title'>Login</h2>
-                    <form action="">
+
+                    <form onSubmit={handleLoginSubmit}>
                         <div className='input-group'>
                             <label htmlFor="email">Email</label>
-                            <input type="email" name='email' placeholder='Enter your email' required />
+                            <input ref={emailRef} type="email" name='email' placeholder='Enter your email' required />
                         </div>
 
                         <div className='input-group'>
                             <label htmlFor="password">Password</label>
-                            <input type="email" name='password' placeholder='Enter your password' required />
+                            <input ref={passwordRef} type="password" name='password' placeholder='Enter your password' required />
                         </div>
+
+                        <p style={{ color: "red" }}>{error?.message}</p>
+                        {
+                            loading && <p>Loading...</p>
+                        }
                         <input className='form-submit' type="submit" value="Login" />
                     </form>
-                    <p>New to simple-E-commerce? <Link className='form-link' to="/signUp">Create an account</Link></p>
+                    <p>New on simple-E-commerce? <Link className='form-link' to="/signUp">Create an account</Link></p>
                 </div>
             </div>
         </div>
--- src/Components/SingUp/SingUp.js
+++ src/Components/SingUp/SingUp.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+
 
 const SingUp = () => {
+    const [errorMassage, setErrorMassage] = useState('');
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const ConfirmPasswordRef = useRef();
+
+    const navigate = useNavigate();
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleSignUpSubmit = (event) => {
+        event.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPassword = ConfirmPasswordRef.current.value;
+
+        if (password !== confirmPassword) {
+            setErrorMassage('Your password did not match');
+            return;
+        };
+        if (password.length < 6) {
+            setErrorMassage('password must be 6 characters of longer');
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password);
+    };
+
+    if (user) {
+        navigate('/shop')
+    }
 
     return (
         <div className='form-container'>
             <div className='input-container'>
                 <div>
                     <h2 className='form-title'>Sing Up</h2>
-                    <form action="">
+
+                    <form onSubmit={handleSignUpSubmit}>
                         <div className='input-group'>
                             <label htmlFor="email">Email</label>
-                            <input type="email" name='email' placeholder='Enter your email' required />
+                            <input ref={emailRef} type="email" name='email' placeholder='Enter your email' required />
                         </div>
 
                         <div className='input-group'>
                             <label htmlFor="password">Password</label>
-                            <input type="email" name='password' placeholder='Enter your password' required />
+                            <input ref={passwordRef} type="password" name='password' placeholder='Enter your password' required />
                         </div>
+
+                        <div className='input-group'>
+                            <label htmlFor="confirm-password">Conform Password</label>
+                            <input ref={ConfirmPasswordRef} type="password" name='confirm-password' placeholder='Enter your password' required />
+                        </div>
+                        <p style={{ color: "red" }}>{errorMassage}</p>
+                        {
+                            loading && <p>Loading...</p>
+                        }
                         <input className='form-submit' type="submit" value="Sign Up" />
                     </form>
-                    <p>Alradi have an account? <Link className='form-link' to="/login">Go to login page</Link></p>
+                    <p>Already have an account? <Link className='form-link' to="/login">login page</Link></p>
                 </div>
             </div>
         </div>
