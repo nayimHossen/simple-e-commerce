385e3c204a15d48f28edf2b6e963f4fc27230660
Nayim hossen <nayimh2015@gmail.com>
2022-04-28 23:51:47 +0600

    shipment page and info implement

--- src/Components/Shipment/Shipment.js
+++ src/Components/Shipment/Shipment.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
+    const nameRef = useRef();
+    const addressRef = useRef();
+    const phoneRef = useRef();
+
+    const handleSignUpSubmit = (event) => {
+        event.preventDefault();
+
+        const name = nameRef.current.value;
+        const address = addressRef.current.value;
+        const phone = phoneRef.current.value;
+
+        const userShipmentAddress = { name, address, phone };
+
+        console.log(userShipmentAddress);
+
+    };
+
     return (
-        <div>
-            <h2>This is shipment</h2>
+        <div className='form-container'>
+            <div className='input-container'>
+                <div>
+                    <h2 className='form-title'>Shipment Information</h2>
+
+                    <form onSubmit={handleSignUpSubmit}>
+                        <div className='input-group'>
+                            <label htmlFor="email">Name</label>
+                            <input ref={nameRef} type="text" name='name' placeholder='your name' required />
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor="password">Email</label>
+                            <input value={user?.email} readOnly type="text" name='password' placeholder='your email' required />
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor="password">Address</label>
+                            <input ref={addressRef} type="text" name='address' placeholder='your address' required />
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor="phone">Phone Number</label>
+                            <input ref={phoneRef} type="number" name='phone' placeholder='your phone number' required />
+                        </div>
+
+                        <input className='form-submit' type="submit" value="Sign Up" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
