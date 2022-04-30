425f9d2afb7673b8451956fc9ca1b98bda19c77c
Nayim hossen <nayimh2015@gmail.com>
2022-04-30 19:26:51 +0600

    pagination implemented

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,12 +3,31 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css';
-import useProducts from '../../Hooks/useProducts';
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
-    const [products] = useProducts();
     const [cart, setCart] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/products?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [page, size]);
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/productsCount`)
+            .then(res => res.json())
+            .then(data => {
+                const count = data.count;
+                const pages = Math.ceil(count / 10);
+                setPageCount(pages);
+            })
+    }, []);
 
     useEffect(() => {
         const storedCart = getStoredCart();
@@ -32,14 +51,35 @@ const Shop = () => {
 
     return (
         <div className='shop-container'>
-            <div className="product-container">
-                {
-                    products.map(product => <Product
-                        key={product._id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    />)
-                }
+            <div>
+                <div className="product-container">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        />)
+                    }
+                </div>
+
+                <div className='pagination-container'>
+                    <div className="pagination">
+                        {
+                            [...Array(pageCount).keys()]
+                                .map(number => <button
+                                    className={page === number ? "selected" : ''}
+                                    onClick={() => setPage(number)}
+                                >{number + 1}</button>)
+                        }
+                        {size}
+                        <select onChange={e => setSize(e.target.value)}>
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                        </select>
+                    </div>
+                </div>
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
